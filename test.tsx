c14f6286e2954faacb6463745cf9f3d3dd1f70f2
hmd <->
2024-10-12 17:11:07 +0330

    fix: test files

--- test.tsx
+++ test.tsx
@@ -1,9 +1,12 @@
-function Comp(){
-
-return (
-<button className=""
-style={{
-"dkfd": "linear-gradient(45deg,var(--un-preset-radix-red9A),var(--un-preset-radix-orange9))"
-}}
->Delete</button>
-)};
+function Comp() {
+  return (
+    <button
+      className=''
+      style={{
+        dkfd: 'linear-gradient(45deg,var(--un-preset-radix-red9A),var(--un-preset-radix-orange9))',
+      }}
+    >
+      Delete
+    </button>
+  );
+}
